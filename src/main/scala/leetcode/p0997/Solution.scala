e0f1275e24a523b9b5e65d0760e38403b0cbb2e6
jang <dragoalie@gmail.com>
2019-05-27 08:22:15 +0900

    solve using seqlike distinct

--- src/main/scala/leetcode/p0997/Solution.scala
+++ src/main/scala/leetcode/p0997/Solution.scala
@@ -10,4 +10,19 @@ object Solution {
     }
   }
 
+  def findJudge_(N: Int, trust: Array[Array[Int]]): Int =
+    trust.map(_ (0)).distinct match {
+      case citizen if citizen.length == N - 1 =>
+        ((1 to N) diff citizen).head match {
+          case judge if trust.count(_ (1) == judge) == N - 1 => judge
+          case _ => -1
+        }
+      case _ => -1
+    }
+
+
+  def main(args: Array[String]) {
+    println(findJudge_(3, Array(Array(1, 3), Array(2, 3), Array(2, 1), Array(1, 2))))
+  }
+
 }
--- src/test/scala/p0997/SolutionSpec.scala
+++ src/test/scala/p0997/SolutionSpec.scala
@@ -1,6 +1,6 @@
 package p0997
 
-import leetcode.p0997.Solution.findJudge
+import leetcode.p0997.Solution._
 import org.scalatest.{FunSpec, Matchers}
 
 class SolutionSpec extends FunSpec with Matchers {
@@ -12,9 +12,9 @@ class SolutionSpec extends FunSpec with Matchers {
       val trust = Array.empty[Array[Int]]
 
       val expected = 1
-      val actual = findJudge(N, trust)
 
-      assert(expected == actual)
+      assert(expected == findJudge(N, trust))
+      assert(expected == findJudge_(N, trust))
     }
 
     it("should return right hand side value if there only two people") {
@@ -22,9 +22,9 @@ class SolutionSpec extends FunSpec with Matchers {
       val trust = Array(Array(1, 2))
 
       val expected = 2
-      val actual = findJudge(N, trust)
 
-      assert(expected == actual)
+      assert(expected == findJudge(N, trust))
+      assert(expected == findJudge_(N, trust))
     }
 
     it("should return a judge if judge trusted by everyone except judge itself AND judge trust nobody") {
@@ -32,9 +32,9 @@ class SolutionSpec extends FunSpec with Matchers {
       val trust = Array(Array(1, 3), Array(2, 3), Array(2, 1), Array(1, 2))
 
       val expected = 3
-      val actual = findJudge(N, trust)
 
-      assert(expected == actual)
+      assert(expected == findJudge(N, trust))
+      assert(expected == findJudge_(N, trust))
     }
 
     it("should return a judge if judge trusted by everyone except judge itself AND judge trust nobody in 4 people") {
@@ -42,9 +42,9 @@ class SolutionSpec extends FunSpec with Matchers {
       val trust = Array(Array(1, 3), Array(1, 4), Array(2, 3), Array(2, 4), Array(4, 3))
 
       val expected = 3
-      val actual = findJudge(N, trust)
 
-      assert(expected == actual)
+      assert(expected == findJudge(N, trust))
+      assert(expected == findJudge_(N, trust))
     }
 
     it("should NOT return a judge if judge trusted by not everyone") {
@@ -52,9 +52,9 @@ class SolutionSpec extends FunSpec with Matchers {
       val trust = Array(Array(1, 3), Array(1, 4), Array(2, 3), Array(2, 4))
 
       val expected = -1
-      val actual = findJudge(N, trust)
 
-      assert(expected == actual)
+      assert(expected == findJudge(N, trust))
+      assert(expected == findJudge_(N, trust))
     }
 
     it("should NOT return a judge if judge trusted by everyone except judge itself BUT judge trust someone") {
@@ -62,9 +62,9 @@ class SolutionSpec extends FunSpec with Matchers {
       val trust = Array(Array(1, 3), Array(2, 3), Array(3, 1))
 
       val expected = -1
-      val actual = findJudge(N, trust)
 
-      assert(expected == actual)
+      assert(expected == findJudge(N, trust))
+      assert(expected == findJudge_(N, trust))
     }
 
     it("should NOT return a judge if there is not enough trust") {
@@ -72,9 +72,9 @@ class SolutionSpec extends FunSpec with Matchers {
       val trust = Array(Array(1, 2), Array(2, 3))
 
       val expected = -1
-      val actual = findJudge(N, trust)
 
-      assert(expected == actual)
+      assert(expected == findJudge(N, trust))
+      assert(expected == findJudge_(N, trust))
     }
 
   }
