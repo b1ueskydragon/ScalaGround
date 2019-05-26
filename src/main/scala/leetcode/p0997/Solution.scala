6c32861f9e631f576231550430ed3a7ff01258f5
jang <dragoalie@gmail.com>
2019-05-26 19:05:22 +0900

    Replace diff to minus to entire n sum

--- src/main/scala/leetcode/p0997/Solution.scala
+++ src/main/scala/leetcode/p0997/Solution.scala
@@ -4,9 +4,10 @@ object Solution {
 
   def findJudge(N: Int, trust: Array[Array[Int]]): Int = trust.transpose match {
     case Array() => 1
-    case tA => tA.last.toSet diff tA.head.toSet match {
-      case group if group.size == 1 && tA.last.count(_ == group.head) == N - 1 => group.head
+    case tA => (1 + N) * N / 2 - tA.head.toSet.sum match {
+      case judge if tA.last.count(_ == judge) == N - 1 => judge
       case _ => -1
     }
   }
+
 }
