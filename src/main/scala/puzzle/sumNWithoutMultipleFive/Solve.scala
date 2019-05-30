1e331fcac8ae69511a12ef2d8b992bf3680f5095
jang <dragoalie@gmail.com>
2019-05-31 08:51:14 +0900

    Refactor to no assign

--- src/main/scala/puzzle/sumNWithoutMultipleFive/Solve.scala
+++ src/main/scala/puzzle/sumNWithoutMultipleFive/Solve.scala
@@ -4,14 +4,6 @@ object Solve {
 
   def sumNWithoutMultipleFive(n: Int): Int = (1 to n).filter(_ % 5 != 0).sum
 
-  def sumNWithoutMultipleFive_(n: Int): Int = {
-
-    val sum: Int = (1 + n) * n / 2
-    val fives: Int = n / 5
-    val fivesSum: Int = ((1 + fives) * 5) * (n / 5) / 2
-
-    sum - fivesSum
-
-  }
+  def sumNWithoutMultipleFive_(n: Int): Int = (1 + n) * n / 2 - (1 + n / 5) * 5 * (n / 5) / 2
 
 }
