6e565f536983809d3d6a5315c4de2e731b61be0e
jang <dragoalie@gmail.com>
2019-06-17 10:14:51 +0900

    solve - immutable map and collect

--- src/main/scala/leetcode/p0001/Solution.scala
+++ src/main/scala/leetcode/p0001/Solution.scala
@@ -4,7 +4,7 @@ package leetcode.p0001
   * and you may not use the same element twice
   */
 object Solution {
-  def twoSum(nums: Array[Int], target: Int): Array[Int] = {
+  def twoSum_(nums: Array[Int], target: Int): Array[Int] = {
     val halfs = nums.zipWithIndex.filter(_._1 * 2 == target)
     if (halfs.length == 2) halfs.map(_._2)
     else {
@@ -14,4 +14,12 @@ object Solution {
       }.map(_._2)
     }
   }
+
+  def twoSum(nums: Array[Int], target: Int): Array[Int] = {
+    val dict = nums.zipWithIndex.toMap
+    nums.zipWithIndex.collectFirst { // filter with map and return found at first
+      case (v, i) if dict.contains(target - v) && dict(target - v) != i => (i, dict(target - v))
+    }.map { case (a, b) => Array(a, b) }.getOrElse(Array(-1))
+  }
+
 }
