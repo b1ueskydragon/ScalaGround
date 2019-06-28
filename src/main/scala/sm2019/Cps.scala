9b39c0b2eb280d13cbd7a7dc04c136928c858607
jang <dragoalie@gmail.com>
2019-06-28 22:26:51 +0900

    Use cont

--- src/main/scala/sm2019/Cps.scala
+++ src/main/scala/sm2019/Cps.scala
@@ -1,15 +1,37 @@
 package sm2019
 
-/** f is subsequent processing */
+/** k is subsequent processing */
 object Cps {
 
-  def multipleFive[A](v: Int, f: Int => A): A = f(v * 5)
+  def multipleFive[A](v: Int, k: Int => A): A = k(v * 5)
 
-  def addTwo[A](v: Int, f: Int => A): A = f(v + 2)
+  def addTwo[A](v: Int, k: Int => A): A = k(v + 2)
 
-  def main(args: Array[String]): Unit = {
-    // println is subsequent processing that passed by param
-    addTwo(10, multipleFive(_, println)) // 60
-  }
+}
+
+case class Cont[R, A](run: (A => R) => R) {
+  def map[B](f: A => B): Cont[R, B] = Cont(k => run(a => k(f(a))))
+
+  def flatMap[B](f: A => Cont[R, B]): Cont[R, B] = Cont(k => run(a => f(a).run(k)))
+}
+
+object Cont {
+  def unit[R, A](a: A): Cont[R, A] = new Cont(k => k(a))
+}
+
+object Main extends App {
+
+  import Cps._
+
+  val v = 10
+
+  addTwo(v, multipleFive(_, println))
+
+  val cont = for {
+    m <- Cont.unit(v)
+    n <- Cont(addTwo[Unit](m, _))
+    l <- Cont(multipleFive[Unit](n, _))
+  } yield l
+  cont.run(println)
 
 }
