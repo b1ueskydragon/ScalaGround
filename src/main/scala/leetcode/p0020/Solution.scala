6517a138c4143c3444a512ab010d04f271cb96cf
jang <dragoalie@gmail.com>
2019-06-30 14:32:53 +0900

    Refactor

--- src/main/scala/leetcode/p0020/Solution.scala
+++ src/main/scala/leetcode/p0020/Solution.scala
@@ -2,15 +2,17 @@ package leetcode.p0020
 
 object Solution {
   def isValid(s: String): Boolean = {
+    val rules = Map('(' -> ')', '[' -> ']', '{' -> '}')
+
     @scala.annotation.tailrec
-    def openStack(s: List[Char], stack: List[Char], rules: Map[Char, Char]): List[Char] =
+    def openStack(s: List[Char], stack: List[Char]): List[Char] =
       s match {
         case Nil => stack
-        case h :: tail if rules.contains(h) => openStack(tail, h :: stack, rules)
-        case h :: tail if stack.nonEmpty && h == rules(stack.head) => openStack(tail, stack.tail, rules)
+        case h :: tail if rules.contains(h) => openStack(tail, h :: stack)
+        case h :: tail if stack.nonEmpty && h == rules(stack.head) => openStack(tail, stack.tail)
         case _ => '#' :: stack
       }
 
-    openStack(s.toCharArray.toList, Nil, Map('(' -> ')', '[' -> ']', '{' -> '}')).isEmpty
+    openStack(s.toList, Nil).isEmpty
   }
 }
