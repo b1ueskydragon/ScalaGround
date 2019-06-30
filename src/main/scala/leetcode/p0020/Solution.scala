86dcc76fb55054d63ae40782de1b30475486cfea
jang <dragoalie@gmail.com>
2019-06-30 12:57:43 +0900

    solve with immutable stack, tail rec

--- src/main/scala/leetcode/p0020/Solution.scala
+++ src/main/scala/leetcode/p0020/Solution.scala
@@ -2,6 +2,15 @@ package leetcode.p0020
 
 object Solution {
   def isValid(s: String): Boolean = {
-    ???
+    @scala.annotation.tailrec
+    def openStack(s: List[Char], stack: List[Char], rules: Map[Char, Char]): List[Char] =
+      s match {
+        case Nil => stack
+        case h :: tail if rules.contains(h) => openStack(tail, h :: stack, rules)
+        case h :: tail if stack.nonEmpty && h == rules(stack.head) => openStack(tail, stack.tail, rules)
+        case _ => '#' :: stack
+      }
+
+    openStack(s.toCharArray.toList, Nil, Map('(' -> ')', '[' -> ']', '{' -> '}')).isEmpty
   }
 }
