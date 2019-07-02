6d0399f8dea0f42f7dff6c1f62002107e2de6043
ina <dragoalie@gmail.com>
2019-07-03 08:41:42 +0900

    Fix test Cont

--- src/test/scala/sm2019/ContSpec.scala
+++ src/test/scala/sm2019/ContSpec.scala
@@ -7,9 +7,23 @@ class ContSpec extends FunSpec {
 
   describe("unit") {
 
-    it("returns new Instance of Cont itself") {
+    it("returns a new Cont that has a function") {
       val a = 99
-      assert(unit[String, Int](a).isInstanceOf[Cont[String, Int]])
+      assert(unit[String, Int](a).run(_.toString) === "99")
+    }
+
+  }
+
+  describe("map") {
+
+    it("is a functional composition") {
+      val v = 99
+
+      def f(x: Int) = x + 100
+
+      def k(x: Int) = x.toString
+
+      assert(Cont.unit(v).map(f).run(k) === "199")
     }
 
   }
