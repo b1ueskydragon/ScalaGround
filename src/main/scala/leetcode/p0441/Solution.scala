c9bf4ca45fac7690ed902c41aae7d33501848a3b
ina <dragoalie@gmail.com>
2019-07-13 11:01:38 +0900

    Solve with math with convert long when calculate

--- src/main/scala/leetcode/p0441/Solution.scala
+++ src/main/scala/leetcode/p0441/Solution.scala
@@ -1,16 +1,27 @@
 package leetcode.p0441
 
 object Solution {
+
+  /** Pure math
+    *
+    * n.toLong prevents Integer overflow
+    *
+    * @param n coins
+    * @return final step that completed
+    */
+  def arrangeCoins(n: Int): Int = ((Math.sqrt(1 + 8 * n.toLong) - 1) * 0.5).toInt
+
   /** Linear search
     *
     * @param n coins
     * @return final step that completed
     */
-  def arrangeCoins(n: Int): Int = {
+  def arrangeCoins_(n: Int): Int = {
     (1 to n).foldLeft((0L, 0)) { (tup, x) =>
       val (sum, k) = tup
-      val nextSum: Long = sum + x // why Long? since may cause Integer overflow
+      val nextSum = sum + x // why Long? since may cause Integer overflow
       if (sum <= n) (nextSum, k + 1) else (nextSum, k)
     }._2 - 1
   }
+
 }
--- src/test/scala/leetcode/p0441/SolutionSpec.scala
+++ src/test/scala/leetcode/p0441/SolutionSpec.scala
@@ -1,7 +1,7 @@
 package leetcode.p0441
 
-import org.scalatest.FunSpec
 import leetcode.p0441.Solution._
+import org.scalatest.FunSpec
 
 class SolutionSpec extends FunSpec {
 
@@ -11,12 +11,20 @@ class SolutionSpec extends FunSpec {
       assert(arrangeCoins(5) === 2)
       assert(arrangeCoins(8) === 3)
       assert(arrangeCoins(1804289383) === 60070)
+
+      assert(arrangeCoins_(5) === 2)
+      assert(arrangeCoins_(8) === 3)
+      assert(arrangeCoins_(1804289383) === 60070)
     }
 
     it("returns final step that completed if there are n enough coins") {
       assert(arrangeCoins(3) === 2)
       assert(arrangeCoins(6) === 3)
       assert(arrangeCoins(10) === 4)
+
+      assert(arrangeCoins_(3) === 2)
+      assert(arrangeCoins_(6) === 3)
+      assert(arrangeCoins_(10) === 4)
     }
 
   }
