43a229ace4b28925aea10adf565b247f54d7d646
ina <dragoalie@gmail.com>
2019-07-13 11:44:06 +0900

    Solve with substrack coins by tailrec, Fix docs

--- src/main/scala/leetcode/p0441/Solution.scala
+++ src/main/scala/leetcode/p0441/Solution.scala
@@ -2,21 +2,27 @@ package leetcode.p0441
 
 object Solution {
 
-  /** Pure math
-    *
-    * n.toLong prevents Integer overflow
+  /** Pure math.
+    * n.toLong (toDouble, 64bit numeric type anyway) prevents Integer overflow
     *
     * @param n coins
     * @return final step that completed
     */
   def arrangeCoins(n: Int): Int = ((Math.sqrt(1 + 8 * n.toLong) - 1) * 0.5).toInt
 
-  /** Linear search
-    *
-    * @param n coins
-    * @return final step that completed
-    */
+  /** Recursion with subtract */
   def arrangeCoins_(n: Int): Int = {
+    /** @param n n coins
+      * @param k k coins that each row has
+      * @return k
+      */
+    @scala.annotation.tailrec def rec(n: Int, k: Int): Int = if (n > 0) rec(n - k, k + 1) else if (n == 0) k - 1 else k - 2
+
+    rec(n, 1)
+  }
+
+  /** Linear search */
+  def arrangeCoinsSlow(n: Int): Int = {
     (1 to n).foldLeft((0L, 0)) { (tup, x) =>
       val (sum, k) = tup
       val nextSum = sum + x // why Long? since may cause Integer overflow
