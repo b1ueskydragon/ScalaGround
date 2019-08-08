dacb5051a1cc627d568fb43b8622644a1fca36ff
ina <dragoalie@gmail.com>
2019-08-09 00:49:31 +0900

    Replace to option

--- src/main/scala/graffiti/binarySearch/orderAgnostic/Solution.scala
+++ src/main/scala/graffiti/binarySearch/orderAgnostic/Solution.scala
@@ -3,15 +3,15 @@ package graffiti.binarySearch.orderAgnostic
 object Solution {
   def searchKeyIndexFromSortedArray(ary: Array[Int], key: Int): Int = {
     @scala.annotation.tailrec
-    def search(low: Int, high: Int, as: Array[Int]): Int = {
-      if (low > high) return -1
+    def search(low: Int, high: Int, as: Array[Int]): Option[Int] = {
+      if (low > high) return None
 
       val middle = low + (high - low) / 2
       val midVal = as(middle)
-      if (midVal == key) middle else if (midVal > key) search(low, middle - 1, as) else search(middle + 1, high, as)
+      if (midVal == key) Some(middle) else if (midVal > key) search(low, middle - 1, as) else search(middle + 1, high, as)
     }
 
-    search(0, ary.length - 1, ary)
+    search(0, ary.length - 1, ary).getOrElse(-1)
   }
 
 }
