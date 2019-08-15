78d6aa7b8db2456b1b44620140e6ae83b6bb30db
ina <dragoalie@gmail.com>
2019-08-15 20:52:08 +0900

    Rename to explicit move

--- src/main/scala/graffiti/Hanoi.scala
+++ src/main/scala/graffiti/Hanoi.scala
@@ -8,43 +8,43 @@ object Hanoi {
     * @param y peg to go
     * @param z peg remain
     */
-  def hanoi(n: Int, x: String, y: String, z: String): Unit = {
+  def move(n: Int, x: String, y: String, z: String): Unit = {
     if (n > 0) {
-      hanoi(n - 1, x, z, y)
+      move(n - 1, x, z, y)
       println(s"""$n, $x -> $y""") // move a biggest one, left to middle (base plate)
-      hanoi(n - 1, z, y, x)
+      move(n - 1, z, y, x)
     }
   }
 
-  def movef(n: Int, from: String, to: String, via: String): Unit = {
+  def move_(n: Int, from: String, to: String, via: String): Unit = {
     if (n == 1) {
       println(s"$from -> $to")
     } else {
-      movef(n - 1, from, via, to)
-      movef(1, from, to, via)
-      movef(n - 1, via, to, from)
+      move_(n - 1, from, via, to)
+      move_(1, from, to, via)
+      move_(n - 1, via, to, from)
     }
   }
 
-  def move(pos: Pos, rest: List[Pos]): Unit = {
+  def move___(pos: Pos, rest: List[Pos]): Unit = {
     val Pos(n, from, to, via) = pos
     if (n == 1) {
       println(s"$from -> $to")
-      if (rest.nonEmpty) move(rest.head, rest.tail)
+      if (rest.nonEmpty) move___(rest.head, rest.tail)
     } else {
       val pos1 = Pos(n - 1, from, via, to)
       val pos2 = Pos(1, from, to, via)
       val pos3 = Pos(n - 1, via, to, from)
-      move(pos1, pos2 :: pos3 :: rest)
+      move___(pos1, pos2 :: pos3 :: rest)
     }
   }
 
   def main(args: Array[String]): Unit = {
-    hanoi(3, "left", "middle", "right")
+    move(3, "left", "middle", "right")
     println()
-    move(Pos(3, "left", "middle", "right"), Nil)
+    move___(Pos(3, "left", "middle", "right"), Nil)
     println()
-    movef(3, "left", "middle", "right")
+    move_(3, "left", "middle", "right")
   }
 
 }
