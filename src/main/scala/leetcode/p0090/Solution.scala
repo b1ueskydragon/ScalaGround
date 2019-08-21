ba24a63133601142ce404f0e99eb6fc3f730481a
ina <dragoalie@gmail.com>
2019-08-22 00:01:34 +0900

    Use built-in combination for skip duplicate

--- src/main/scala/leetcode/p0090/Solution.scala
+++ src/main/scala/leetcode/p0090/Solution.scala
@@ -9,4 +9,13 @@ object Solution {
     scala.util.Sorting.quickSort(nums)
     rec(nums.toList, List(List[Int]())).distinct
   }
+
+  def subsetsWithDup_(nums: Array[Int]): List[List[Int]] = {
+    @scala.annotation.tailrec
+    def rec(t: List[Int], n: Int, res: Iterator[List[Int]]): Iterator[List[Int]] =
+      if (n == 0) res else rec(t, n - 1, res ++ t.combinations(n))
+
+    rec(nums.toList, nums.length, Iterator(List[Int]())).toList
+  }
+
 }
--- src/test/scala/leetcode/p0090/SolutionSpec.scala
+++ src/test/scala/leetcode/p0090/SolutionSpec.scala
@@ -35,4 +35,34 @@ class SolutionSpec extends FunSpec {
 
   }
 
+  describe("subsetsWithDup_") {
+
+    it("returns 6 combinations in case [1, 2, 2]") {
+      val nums = Array(1, 2, 2)
+      val expected = List(
+        List(),
+        List(1, 2, 2),
+        List(1, 2),
+        List(2, 2),
+        List(1),
+        List(2)
+      )
+      assert(subsetsWithDup_(nums) === expected)
+    }
+
+    it("returns same result but diff order in case [2, 1, 2]") {
+      val nums = Array(2, 1, 2)
+      val expected = List(
+        List(),
+        List(2, 2, 1),
+        List(2, 2),
+        List(2, 1),
+        List(2),
+        List(1)
+      )
+      assert(subsetsWithDup_(nums) === expected)
+    }
+
+  }
+
 }
