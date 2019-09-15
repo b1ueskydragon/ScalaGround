3f3bdbc8ca2214864779c6a9a247e8783b315605
ina <dragoalie@gmail.com>
2019-09-16 01:45:43 +0900

    Make recursive product function

--- src/main/scala/leetcode/p0784/Solution.scala
+++ src/main/scala/leetcode/p0784/Solution.scala
@@ -6,16 +6,24 @@ sealed case class Letter(
 )
 
 object Solution {
+
+  def product[T](list: List[List[T]]): List[List[T]] = list match {
+    case Nil => Nil
+    case xs :: Nil => xs.map(List(_)) // to list's list
+    case x :: xs => for {
+      i <- x
+      j <- product(xs)
+    } yield List(i) ::: j
+  }
+
   def letterCasePermutation(S: String): List[String] = {
-    val pairs = S.map(s => if (s.isLetter) List(s.toLower, s.toUpper) else List(s))
-    pairs.flatten.combinations(S.length).map(_.mkString).toList // not combination, direct product
+    val pairs = S.map(s => if (s.isLetter) List(s.toLower, s.toUpper) else List(s)).toList
+    product(pairs).map(_.mkString)
   }
 
   def main(args: Array[String]): Unit = {
     val res = letterCasePermutation("ab") // (a, A) (b, B)
     println(res) // ab, aB, Ab, AB
   }
+
 }
-
-
-
