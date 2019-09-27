4aa38793b4e27d3a176b91e813c59d8e8497da17
ina <dragoalie@gmail.com>
2019-09-27 11:19:33 +0900

    Refactor to has sub-structure

--- src/main/scala/graffiti/CartesianProduct.scala
+++ src/main/scala/graffiti/CartesianProduct.scala
@@ -2,49 +2,55 @@ package graffiti
 
 object CartesianProduct {
 
-  /**
-    * product ( { {a, A}, {b, B}, {c, C} } )
-    *
-    * product ( {a, A} :: product ( { {b, B}, {c, C} } ) )
-    */
-  def product[T](xss: Traversable[Traversable[T]]): Traversable[Traversable[T]] = xss match {
-    case Nil => Nil
-    case xs :: Nil => xs.map(List(_)) // only an one list
-    case xs :: tail => xs.flatMap(x => product(tail).map(Traversable(x) ++ _))
-  }
-
-  // recursive; replace flatMap to yielding an element in each for-comprehension
-  def product_[T](xss: Traversable[Traversable[T]]): Traversable[Traversable[T]] = xss match {
-    case Nil => Nil
-    case xs :: Nil => xs.map(List(_)) // only an one list
-    case xs :: tail => for {
-      m <- xs
-      n <- product_(tail)
-    } yield Traversable(m) ++ n
-  }
-
-  // non-recursive
-  // Traversable.empty[Traversable[T]] != Traversable(Traversable.empty[T])
-  def product___[T](xss: Traversable[Traversable[T]]): Traversable[Traversable[T]] =
-    xss.foldLeft(Traversable(Traversable.empty[T])) { (acc, xs) =>
-      acc.flatMap { h =>
-        xs.map(x => h ++ Traversable(x))
-      }
+  object Recursive {
+    /**
+      * product ( { {a, A}, {b, B}, {c, C} } )
+      *
+      * product ( {a, A} :: product ( { {b, B}, {c, C} } ) )
+      */
+    def product[T](xss: Traversable[Traversable[T]]): Traversable[Traversable[T]] = xss match {
+      case Nil => Nil
+      case xs :: Nil => xs.map(List(_)) // only an one list
+      case xs :: tail => xs.flatMap(x => product(tail).map(Traversable(x) ++ _))
     }
 
-  def product2[T](xs: Traversable[T], ys: Traversable[T]): Traversable[(T, T)] = xs.flatMap(x => ys.map((x, _)))
+    // replace flatMap to yielding an element in each for-comprehension
+    def productFor[T](xss: Traversable[Traversable[T]]): Traversable[Traversable[T]] = xss match {
+      case Nil => Nil
+      case xs :: Nil => xs.map(List(_)) // only an one list
+      case xs :: tail => for {
+        m <- xs
+        n <- productFor(tail)
+      } yield Traversable(m) ++ n
+    }
+
+  }
+
+  object NonRecursive {
+    // Traversable.empty[Traversable[T]] != Traversable(Traversable.empty[T])
+    def product[T](xss: Traversable[Traversable[T]]): Traversable[Traversable[T]] =
+      xss.foldLeft(Traversable(Traversable.empty[T])) { (acc, xs) =>
+        acc.flatMap { h =>
+          xs.map(x => h ++ Traversable(x))
+        }
+      }
+
+    def product2[T](xs: Traversable[T], ys: Traversable[T]): Traversable[(T, T)] = xs.flatMap(x => ys.map((x, _)))
+  }
+
 
   def main(args: Array[String]): Unit = {
-    println(product(Nil))
+    println(Recursive.product(Nil))
+
     val alist = List(List('a, 'b, 'c))
-    println(product(alist))
+    println(Recursive.product(alist))
 
     val lists = List(List('a, 'b, 'c), List(1, 2, 3))
-    println(product(lists))
-    println(product_(lists))
-    println(product___(lists))
+    println(Recursive.product(lists))
+    println(Recursive.productFor(lists))
+    println(NonRecursive.product(lists))
 
-    println(product2(List('y, 'Y), List('z, 'Z)))
+    println(NonRecursive.product2(List('y, 'Y), List('z, 'Z)))
   }
 
 }
