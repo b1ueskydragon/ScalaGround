2de880e2cd95924a9cd096204ad8bbeca4f9f983
ina <dragoalie@gmail.com>
2020-02-14 11:55:51 +0900

    Minor refactor

--- src/main/scala/graffiti/TriangleInequality.scala
+++ src/main/scala/graffiti/TriangleInequality.scala
@@ -2,20 +2,22 @@ package graffiti
 
 object TriangleInequality {
 
-  private def sum(x: Int, y: Int, z: Int) = x + y + z
+  private def sum(l: Int*) = l.sum
 
-  private def max(x: Int, y: Int, z: Int) = Math.max(Math.max(x, y), z)
+  private def max(l: Int*) = l.foldLeft(0)((res, x) => if (res > x) res else x)
 
   // x, y, z | if z is max | x + y > z
   private def isTriangle(x: Int, y: Int, z: Int) = sum(x, y, z) > 2 * max(x, y, z)
 
   def main(args: Array[String]): Unit = {
+    assert(isTriangle(1, 1, 1))
     assert(isTriangle(3, 5, 4))
     assert(isTriangle(2, 2, 2))
     assert(isTriangle(6, 8, 7))
     assert(!isTriangle(2, 10, 2))
     assert(!isTriangle(1, 3, 2))
     assert(!isTriangle(8, 4, 4))
+    assert(!isTriangle(100, 10, 10))
   }
 
 }
