4ff1951ae0e6e61da91adc97278665a87b32bb3c
ina <dragoalie@gmail.com>
2020-02-16 16:50:55 +0900

    solved - currInc should be updated

--- src/main/scala/leetcode/p0198/Solution.scala
+++ src/main/scala/leetcode/p0198/Solution.scala
@@ -4,7 +4,7 @@ object Solution {
   def rob(nums: Array[Int]): Int = loop(nums, 0, 0, 0, 0)
 
   @scala.annotation.tailrec
-  def loop(nums: Array[Int], currInc: Int, currExc: Int, prevInc: Int, prevExc: Int): Int = {
+  def loop(nums: Array[Int], currInc: Int, currExc: Int, prevInc: Int, prevExc: Int): Int =
     if (nums.isEmpty) Math.max(currExc, currInc)
     else {
       /*
@@ -12,7 +12,6 @@ object Solution {
        *                      prevExc := a1 + a3        or a1 + a4 or a2 + a4
        *                              := a5's prevExc           a5's prevInc
        */
-      loop(nums.tail, nums.head + prevExc, prevInc, currInc, Math.max(prevExc, prevInc))
+      loop(nums.tail, nums.head + prevExc, prevInc, nums.head + prevExc, Math.max(prevExc, prevInc))
     }
-  }
 }
