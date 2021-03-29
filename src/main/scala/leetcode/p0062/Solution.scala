144993b386e936acfd38f69bf029efcdce639958
ina <dragoalie@gmail.com>
2021-03-29 23:28:19 +0900

    solved - DP, For comprehension, Array. unique paths

---
+++ src/main/scala/leetcode/p0062/Solution.scala
@@ -0,0 +1,13 @@
+package leetcode.p0062
+
+object Solution {
+  def uniquePaths(m: Int, n: Int): Int = {
+    val ary = Array.fill(m, n)(0)
+    for {
+      i <- 0 until m
+      j <- 0 until n
+    } yield ary(i)(j) = if (i == 0 || j == 0) 1 else ary(i - 1)(j) + ary(i)(j - 1)
+
+    ary(m - 1)(n - 1)
+  }
+}
