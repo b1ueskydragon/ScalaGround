5352de54186646c65c4e99b67972c472ccdc2249
ina <dragoalie@gmail.com>
2021-06-30 10:03:34 +0900

    solved - simple recursive dfs. Invert Binary Tree

---
+++ src/main/scala/leetcode/p0226/dfs/recursive/Solution.scala
@@ -0,0 +1,21 @@
+package leetcode.p0226.dfs.recursive
+
+private class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null) {
+  var value: Int = _value
+  var left: TreeNode = _left
+  var right: TreeNode = _right
+}
+
+object Solution {
+  def invertTree(root: TreeNode): TreeNode = {
+    if (root == null) return null
+
+    val left = invertTree(root.left)
+    val right = invertTree(root.right)
+
+    root.left = right
+    root.right = left
+
+    root
+  }
+}
